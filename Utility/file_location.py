b589ff242fc795e8f1597af7639c66ca08685b3b
oren berkovich <orenber@hotmail.com>
2021-06-29 12:47:10 +0300

    1.unitest 2.file_date

--- Utility/file_location.py
+++ Utility/file_location.py
@@ -1,5 +1,7 @@
 import os
-import datetime
+
+import logging
+from datetime import datetime
 
 
 def full_file(file_folder: list = ()) -> str:
@@ -7,12 +9,32 @@ def full_file(file_folder: list = ()) -> str:
 
 
 def file_date(file_name: str = "output", extension: str = " ") -> str:
-    current_date_and_time = datetime.datetime.now()
+
+    current_date_and_time = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
     current_date_and_time_string = str(current_date_and_time)
 
-    file_full_name = file_name + " " + current_date_and_time_string + extension
+    file_full_name = file_name + "_" + current_date_and_time_string + extension
     return file_full_name
 
+
+class LOGGER:
+    def __int__(self, app_name: str):
+        logging.basicConfig(level=logging.INFO, filename= file_date(app_name, '.log'), filemode='w')
+        self.logger = logging.getLogger(app_name+"Start")
+        self.logger.setLevel(logging.DEBUG)
+
+        # create console handler and set level to debug
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+
+        # create formatter
+        formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s")
+
+        # add formatter to ch
+        ch.setFormatter(formatter)
+
+        # add ch to logger
+        self.logger.addHandler(ch)
 
 def main():
 
