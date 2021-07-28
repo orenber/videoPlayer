42c683295f4fd3f7c2f21c8a815c005e35ba0058
oren berkovich <orenber@hotmail.com>
2021-07-28 10:36:27 +0300

    1.update trainer script

--- GUI/trainer.py
+++ GUI/trainer.py
@@ -1,9 +1,38 @@
 from GUI.dynamic_panel import DynamicPanel
 from tkinter import *
+from tkinter import ttk
 
 
 root =Tk()
 root.geometry("500x700+0+0")
+
+# create Main Frame
+main_frame = Frame(root)
+main_frame.pack(fill=BOTH, expand=1)
+
+# create canvas
+my_canvas = Canvas(main_frame)
+my_canvas.pack(side=LEFT, fill=BOTH, expand=1)
+
+
+# add scrollbar to the canvas
+my_scroll_bar = ttk.Scrollbar(main_frame,
+                              orient=VERTICAL,
+                              command=my_canvas.yview)
+my_scroll_bar.pack(side=RIGHT, fill=Y)
+
+# Configure the canvas
+my_canvas.configure(yscrollcommand=my_scroll_bar)
+my_canvas.bind('<Configure>', lambda e: my_canvas)
+
+# build to frame control
+
+# build open filfe folder button
+
+# build frame images
+
+# build scrollbar
+
 # image gallery
 matrix = {"row": [{"col": [0, 0, 0,0, 0]},
                   {"col": [1, 1, 1,1,1]},
@@ -12,6 +41,10 @@ matrix = {"row": [{"col": [0, 0, 0,0, 0]},
                   {"col": [4, 4, 4,4,4]},
                   ]}
 pan = DynamicPanel(root, matrix )
+
+# build to frame control
+
+# build open file folder folder button
 
 pan.mainloop()
 # sliding bar
--- Utility/file_location.py
+++ Utility/file_location.py
@@ -1,5 +1,4 @@
 import os
-import logging
 from datetime import datetime
 
 
@@ -21,26 +20,6 @@ def create_folder_if_not_exist(file_folder: str = os.pardir) -> bool:
     os.makedirs(file_folder, exist_ok=True)
     is_exist = os.path.isdir(file_folder)
     return is_exist
-
-
-class LOGGER:
-    def __int__(self, app_name: str):
-        logging.basicConfig(level=logging.INFO, filename=file_date(app_name, '.log'), filemode='w')
-        self.logger = logging.getLogger(app_name+"Start")
-        self.logger.setLevel(logging.DEBUG)
-
-        # create console handler and set level to debug
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.DEBUG)
-
-        # create formatter
-        formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s")
-
-        # add formatter to ch
-        ch.setFormatter(formatter)
-
-        # add ch to logger
-        self.logger.addHandler(ch)
 
 
 def main():
