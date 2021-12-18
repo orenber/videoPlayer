020a3bd05e74f7d8bb277d6bb13ed9affc7ac5a8
orenber <orenber@hotmail.com>
2021-12-19 00:37:27 +0200

    1. center the widget 2. fix splash screen bug 3. redisign training

--- Algo/face_trainer.py
+++ Algo/face_trainer.py
@@ -1,16 +1,25 @@
 
-from Utility.file_location import *
-from Utility.color_names import COLOR
-import cv2
-from PIL import Image
-import numpy as np
 import pickle
+
+import cv2
+import numpy as np
+from PIL import Image
+
+from Utility.color_names import COLOR
+from Utility.file_location import *
+from Utility.logger_setup import setup_logger
+
+try:
+    from Algo.detect_mask_video import MaskDetection
+except Exception as error:
+    print(error)
 
 
 class FaceTrainer:
 
     def __init__(self):
 
+        self.log = setup_logger( 'Trainer' )
         self.recognizer = cv2.face.LBPHFaceRecognizer_create()
         self.path = os.path.dirname( cv2.__file__ )
         self.face_frontal_path = os.path.join( self.path, 'data', 'haarcascade_frontalface_default.xml' )
@@ -34,6 +43,11 @@ class FaceTrainer:
         self._confident = 45
         self._training = False
         self._image_size = (200, 200)
+
+        try:
+            self.mask_detector = MaskDetection()
+        except Exception as error:
+            self.log.exception(error)
 
     @property
     def training(self) -> bool:
@@ -134,7 +148,7 @@ class FaceTrainer:
             labels = {v: k for k, v in og_labels.items()}
         return labels
 
-    def face_detection(self, gray_image: np.array):
+    def face_detection(self, gray_image: np.ndarray, frame_draw:np.ndarray):
 
         # detect the faces
         faces = self.face_cascade.detectMultiScale( gray_image, 1.1, 4 )
@@ -151,11 +165,11 @@ class FaceTrainer:
 
             self.face[count] = {'detect': True, 'ROI': {'x': (x, x + w), 'y': (y, y + h)},
                                 'pos_label': (x + 6, y - 6)}
-            cv2.rectangle( self.frame.image, (x, y), (x + w, y + h), COLOR['blue'], 2 )
-            cv2.putText( self.frame.image, 'Face', self.face[count]['pos_label'],
+            cv2.rectangle( frame_draw, (x, y), (x + w, y + h), COLOR['blue'], 2 )
+            cv2.putText(frame_draw, 'Face', self.face[count]['pos_label'],
                          cv2.FONT_HERSHEY_DUPLEX, 0.5, COLOR['green'], 1 )
 
-    def face_recognition(self, gray_image: np.array):
+    def face_recognition(self, gray_image: np.ndarray, frame_draw: np.ndarray):
 
         # detect faces in the image
         faces = self.face_cascade.detectMultiScale( gray_image, scaleFactor=1.5, minNeighbors=5 )
@@ -166,28 +180,28 @@ class FaceTrainer:
             roi_gray = gray_image[y:y + h, x:x + w]
 
             # recognizer deep learned model predict keras tensorflow pytorch scikit learn
-            id_, conf = self.recognizer.predict( roi_gray )
+            id_, conf = self.recognizer.predict(roi_gray)
 
             if conf >= self._confident:
-                name = self.faces_names[id_]
+                name = self.ids_label[id_]
 
-                # take the roi corrdinet x and y
+                # take the roi acorrdinet x and y
                 points_start = (x, y)
                 points_end = (x + w, y + h)
-                cv2.putText( self.frame.image, name, points_start,
-                             cv2.FONT_HERSHEY_SIMPLEX, 1, COLOR['white'], 2, cv2.LINE_AA )
+                cv2.putText(frame_draw, name, points_start,
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, COLOR['white'], 2, cv2.LINE_AA)
                 # draw rectangle around the faces
-                cv2.rectangle( self.frame.image, points_start, points_end, COLOR['blue'], 2 )
+                cv2.rectangle(frame_draw, points_start, points_end, COLOR['blue'], 2)
 
-    def mask_detection(self, rgb_image: np.array):
+    def mask_detection(self, rgb_image: np.ndarray, frame_draw: np.ndarray):
 
         # detect faces in the frame and determine if they are wearing a
         # face mask or not
-        (locs, preds) = self.mask_detector.detect_and_predict_mask( rgb_image )
+        (locs, preds) = self.mask_detector.detect_and_predict_mask(rgb_image)
 
         # loop over the detected face locations and their corresponding
         # locations
-        for (box, pred) in zip( locs, preds ):
+        for (box, pred) in zip(locs, preds):
             # unpack the bounding box and predictions
             (startX, startY, endX, endY) = box
             (mask, withoutMask) = pred
@@ -198,17 +212,16 @@ class FaceTrainer:
             color = COLOR["green"] if label == "Mask" else COLOR["red"]
 
             # include the probability in the label
-            label = "{}: {:.2f}%".format( label, max( mask, withoutMask ) * 100 )
+            label = "{}: {:.2f}%".format( label, max(mask, withoutMask) * 100 )
 
             # display the label and bounding box rectangle on the output
             # frame
-            cv2.putText( self.frame.image, label, (startX, startY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2 )
-            cv2.rectangle( self.frame.image, (startX, startY), (endX, endY), color, 2 )
+            cv2.putText(frame_draw, label, (startX, startY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2 )
+            cv2.rectangle(frame_draw, (startX, startY), (endX, endY), color, 2)
 
 
 def main():
     trainer = FaceTrainer()
-    trainer.mainloop()
 
 
 if __name__ == "__main__":
--- GUI/a_eye_camera.py
+++ GUI/a_eye_camera.py
@@ -4,17 +4,16 @@ from Utility.file_location import *
 from Utility.logger_setup import setup_logger
 from Utility.display_widget import center_widget
 from GUI.splash_screen import Splash
-from time import sleep
 
 
 class AEyeCamera(ttk.Frame):
 
-    def __init__(self, parent: ttk.Frame = None,**kwargs: tuple):
+    def __init__(self, parent: ttk.Frame = None):
         self.log = setup_logger('AEyeCamera')
-        ttk.Frame.__init__(self,parent)
-        self._build_widget(parent)
+        ttk.Frame.__init__(self, parent)
+        self._build_widget()
 
-    def _build_widget(self, parent: ttk.Frame = None, **kwargs: tuple):
+    def _build_widget(self):
 
         self.log.info("start build widget")
         self.splash = Splash()
@@ -26,15 +25,15 @@ class AEyeCamera(ttk.Frame):
         self.splash.splash_root.destroy()
 
         self.master = Tk()
-        self.master.protocol( "WM_DELETE_WINDOW", self.on_closing )
-        self.master.geometry(center_widget(self.master,950,720))
+        self.master.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.master.geometry(center_widget(self.master, 950, 720))
 
         # Title bar Title
         self.master.title("AEye Camera")
         # Title Bar Icon
-        self.icons_path = full_file( ["Icons","webcamera.ico" ])
-        self.master.iconbitmap( self.icons_path)
-        self.notebook = ttk.Notebook( self.master )
+        self.icons_path = full_file(["Icons", "webcamera.ico"])
+        self.master.iconbitmap(self.icons_path)
+        self.notebook = ttk.Notebook(self.master)
         self.main_frame = Frame(width=1000,
                                 height=720,
                                 bg="gray24",
@@ -42,27 +41,29 @@ class AEyeCamera(ttk.Frame):
                                 name="main_frame")
 
         self.training_frame = Frame(width=1000,
-                                     height=720,
-                                     bg="gray24",
-                                     relief="raised",
-                                     name="training_frame" )
+                                    height=720,
+                                    bg="gray24",
+                                    relief="raised",
+                                    name="training_frame")
 
-        self.surveillance = Surveillance( self.main_frame )
-        self.main_frame.pack( fill=BOTH, expand=True )
-        self.notebook.add( self.main_frame, text="Surveillance Camera" )
-        self.trainer = Trainer( self.training_frame )
-        self.training_frame.pack( fill=BOTH, expand=True )
-        self.notebook.add( self.training_frame, text="Training" )
+        self.surveillance = Surveillance(self.main_frame)
+        self.main_frame.pack(fill=BOTH, expand=True)
+        self.notebook.add(self.main_frame, text="Surveillance Camera")
+        self.trainer = Trainer(self.training_frame)
+        self.training_frame.pack(fill=BOTH, expand=True)
+        self.notebook.add(self.training_frame, text="Training")
         self.notebook.pack(fill=BOTH, expand=True)
 
     def on_closing(self):
         if messagebox.askokcancel("Quit", "Do you want to quit?"):
-           self.master.destroy()
-           self.log.info("close main application")
+            self.master.destroy()
+            self.log.info("close main application")
+
 
 def main():
     a_eye = AEyeCamera()
     a_eye.mainloop()
+
 
 if __name__ == '__main__':
     main()
